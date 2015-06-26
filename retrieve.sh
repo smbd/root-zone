0abbc9934d1250d4fe3067f8fd172f790718eee6
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2015-06-26 19:22:56 +0900

    sleep between dig command slough log of git command

--- retrieve.sh
+++ retrieve.sh
@@ -13,10 +13,12 @@ cd `dirname $0` || exit 1
 
 dig ${dig_opt} @${server} -t axfr .| /bin/grep -v '^;' > ${full_out_file}
 
+sleep 30
+
 dig ${dig_opt} @${server} -t axfr .| /bin/grep -v -P "${except_re}" | /bin/grep -v '^;' > ${limited_out_file}
 
 serial=`awk '/\tIN\tSOA\t/{print $7}' ${full_out_file}`
 
-git add ${full_out_file} ${limited_out_file}
-git commit -m "${serial}"
-git push -u origin master 
+git add ${full_out_file} ${limited_out_file} > /dev/null 2>&1
+git commit -m "${serial}" > /dev/null 2>&1
+git push -u origin master > /dev/null 2>&1
